853797068a7a04c2307532e90132c04e2d5457b8
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-05-22 11:34:50 +0200

    artur

--- dades/Persistencia.py
+++ dades/Persistencia.py
@@ -5,41 +5,57 @@ class Persistencia:
 
     @staticmethod
     def eliminar_mesures(nom):
-        with Database() as db:
-            sql = "DELETE FROM mesura WHERE nom=%s"
-            val = [nom]        
-            db.execute(sql,val)    
-            db.commit()
+        try:
+            with Database() as db:
+                sql = "DELETE FROM mesura WHERE nom=%s"
+                val = [nom]        
+                db.execute(sql,val)    
+                db.commit()
+                return True
+        except:
+            print('ERROR en Persistencia.eliminar_mesures')
+            return False
 
     @staticmethod
     def afegir_mesura(nom,x,y):
-        with Database() as db:
-            sql = "INSERT INTO mesura (nom, x, y) VALUES (%s, %s, %s)"
-            val = [nom,x,y]
-            db.execute(sql, val)    
-            db.commit()
+        try:
+            with Database() as db:
+                sql = "INSERT INTO mesura (nom, x, y) VALUES (%s, %s, %s)"
+                val = [nom,x,y]
+                db.execute(sql, val)    
+                db.commit()
+                return True
+        except:
+            print('ERROR en Persistencia.afegir_mesura')
+            return False
+
 
     @staticmethod
     def obtenir_mesures(nom):
         data=[]
-        with Database() as db:
-            sql = "SELECT * FROM mesura WHERE nom=%s"
-            val = [nom]
-            db.execute(sql, val)
-            row = db.fetchone()   
-            while row!=None:
-                data.append(row)
-                row = db.fetchone()    
+        try:
+            with Database() as db:
+                sql = "SELECT * FROM mesura WHERE nom=%s"
+                val = [nom]
+                db.execute(sql, val)
+                row = db.fetchone()   
+                while row!=None:
+                    data.append(row)
+                    row = db.fetchone()    
+        except:
+            print('ERROR en Persistencia.obtenir_mesures')
         return data
 
     @staticmethod
     def obtenir_ultima_mesura(nom):
         row=None
-        with Database() as db:
-            sql = "SELECT * FROM mesura WHERE nom=%s ORDER BY data DESC LIMIT 1"
-            val = [nom]
-            db.execute(sql, val)
-            row = db.fetchone()       
+        try:            
+            with Database() as db:
+                sql = "SELECT * FROM mesura WHERE nom=%s ORDER BY data DESC LIMIT 1"
+                val = [nom]
+                db.execute(sql, val)
+                row = db.fetchone()
+        except:
+            print('ERROR en Persistencia.obtenir_mesures')
+            
         return row
-
-
